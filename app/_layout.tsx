ac2c7f0418ac36db0c52c5d46fefb507fb437031
Aina Kevin <ainakevinramaroson@gmail.com>
2025-08-19 17:40:43 +0300

    Customize Stack navigator header options
    Updated the Stack navigator in RootLayout to set custom header styles, hide the header, and define a title for the index screen. This improves the app's navigation appearance and organization.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack screenOptions={{
+      headerStyle: { backgroundColor: "#16468fff" },
+      headerTintColor: "#070101ff",
+      headerShown : false,
+      headerTitleStyle: { fontWeight: "bold" },
+    }}>
+    <Stack.Screen name="index" options={{ title: "Home" }} />
+  </Stack>
+  );
 }
